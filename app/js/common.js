fc666b88ead27ba9e75edfd0aac58e93ae2f2f0f
seymur-orucov <seymur92@hotmail.com>
2018-09-20 02:19:43 +0400

    20.09.2018

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,18 @@
-$(function() {
+$(document).ready(function (e) {
+	$('.header__burger-button').click(function () { 
+		$('.header__bmenu').fadeToggle(1000);
+	});
 
-	// Custom JS
-
+$(window).scroll(function () { 
+	var scroll = $(window).scrollTop();
+	if (scroll >= 200) {
+		$('.header').addClass('header_bg-dark');
+		$('.header__menu-link').addClass('header__menu-link_gold');
+		$('.header__social-icon').addClass('header__social-icon_gold');
+	} else {
+		$('.header').removeClass('header_bg-dark');
+		$('.header__menu-link').removeClass('header__menu-link_gold');
+		$('.header__social-icon').removeClass('header__social-icon_gold');
+	}
+});
 });
