4789590fcdcfe299acb865a79777cf6c512b8cea
Seymur Orucov <seymur92@hotmail.com>
2018-09-20 11:13:21 +0400

    20.09.2018

--- app/js/common.js
+++ app/js/common.js
@@ -5,7 +5,7 @@ $(document).ready(function (e) {
 
 $(window).scroll(function () { 
 	var scroll = $(window).scrollTop();
-	if (scroll >= 200) {
+	if (scroll >= 900) {
 		$('.header').addClass('header_bg-dark');
 		$('.header__menu-link').addClass('header__menu-link_gold');
 		$('.header__social-icon').addClass('header__social-icon_gold');
