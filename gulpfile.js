6821f2d91dfad336b9e8b74042083c17c558b4e7
Seymur Orucov <seymur92@hotmail.com>
2018-09-21 18:27:22 +0400

    21.09.2018

--- gulpfile.js
+++ gulpfile.js
@@ -64,7 +64,7 @@ gulp.task('rsync', function() {
 gulp.task('watch', ['styles', 'js', 'browser-sync'], function() {
 	gulp.watch('app/'+syntax+'/**/*.'+syntax+'', ['styles']);
 	gulp.watch(['libs/**/*.js', 'app/js/common.js'], ['js']);
-	gulp.watch('app/*.php', browserSync.reload)
+	gulp.watch('app/*.html', browserSync.reload)
 });
 
 gulp.task('default', ['watch']);
